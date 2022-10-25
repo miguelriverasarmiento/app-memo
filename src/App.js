65031a26390aa1625f793c6e384967d52c431cda
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2022-10-25 08:17:55 -0500

    Add quokka to see Traditional Memoization and How React Memoizes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState, memo } from 'react';
 import './App.css';
 
+function Swatch({ color }) {
+  console.log(`Swatch rendered ${color}`)
+  return(
+    <div style={{ margin: 2, width: 75, height: 75, backgroundColor: color }}></div>
+  )
+}
+
+const MemoedSwatch = memo(Swatch);
+
 function App() {
+
+  const [appRenderIndex, setAppRenderIndex] = useState(0);
+  const [color, setColor] = useState("red")
+  console.log(`App rendered ${appRenderIndex}`);
+
   return (
+
     <div className="App">
-      
+      <div>
+        <button onClick={() => setAppRenderIndex(appRenderIndex + 1)}>
+          Re Render-App
+        </button>
+        <button onClick={() => setColor(color === "red" ? "blue" : "red")}>
+          Change Color
+        </button>
+      </div>
+      <div>
+        <MemoedSwatch color={color} />
+      </div>
     </div>
   );
 }
